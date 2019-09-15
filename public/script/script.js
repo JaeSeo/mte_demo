27a77bbf59323fd74fd2aa783c70f50e56e2caf7
jaehyes@JAEHYES.COM <jaehyes@JAEHYES.COM>
2019-09-15 22:23:47 +0900

    baseline

--- public/script/script.js
+++ public/script/script.js
@@ -8,15 +8,18 @@ function colorRemove() {
   $('.inner_cell').removeClass("change");
 }
 
+//on click the cell
 $(".inner_cell").on("click", function() {
+  $('#dynamic_input').remove();  
   let cellId = $(this).attr('value');
+  console.log(cellId);
   $('.field').prepend(`<input type="hidden" id="dynamic_input" name="cellId" value=${cellId}>`);
   $(".popup-overlay, .popup-content").addClass("active");
-  $(".table_body").addClass("background_change");
+  $(".table-body, img").addClass("background_change");
 });
 
 //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
 $(".close_button").on("click", function() {
   $(".popup-overlay, .popup-content").removeClass("active");
-  $(".table_body").removeClass("background_change");
+  $(".table-body, img").removeClass("background_change");
 });
