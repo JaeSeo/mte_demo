608756832f4dfdfb65a3ac14e2afd0f1873d7dfa
Seo <jaehyes@186590d03b81.ant.amazon.com>
2019-10-07 02:44:26 +0900

    new

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const MONGODB_URI = `mongodb+srv://jaehyes:52789ses@cluster0-ckra1.mongodb.net/newTest?retryWrites=true&w=majority`;
+const f = require('util').format;
+const fs = require('fs');
+const ca = [fs.readFileSync("./rds-combined-ca-bundle.pem")];
+const MONGODB_URI = `mongodb://mte:52789ses@docdb-2019-10-06-16-11-57.cluster-c2osdxpx9h2o.ap-northeast-2.docdb.amazonaws.com:27017/test?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0`;
 
 const app = express();
 const store = new MongoDBStore({
@@ -25,7 +28,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(mainRoutes);
 
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB_URI, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    sslValidate: true,
+    sslCA: ca
+  })
   .then(result => {
     app.listen(3000);
   })
