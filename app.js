1a76fa83590b11514ac3582d4432ac92ca1e64f6
Seo <jaehyes@186590d03b81.ant.amazon.com>
2019-10-07 09:59:50 +0900

    newone

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
+//const session = require('express-session');
+//const MongoDBStore = require('connect-mongodb-session')(session);
 const f = require('util').format;
 const fs = require('fs');
 const ca = [fs.readFileSync("./rds-combined-ca-bundle.pem")];
 const MONGODB_URI = `mongodb://mte:52789ses@docdb-2019-10-06-16-11-57.cluster-c2osdxpx9h2o.ap-northeast-2.docdb.amazonaws.com:27017/test?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0`;
 
-const app = express();
-const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions'
-});
+// const app = express();
+// const store = new MongoDBStore({
+//   uri: MONGODB_URI,
+//   collection: 'sessions'
+// });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
